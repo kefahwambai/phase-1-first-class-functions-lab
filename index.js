29ce1f149382843c9d91139535fe190e8f9f0370
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-28 03:07:24 +0300

    Compltet

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const drivers = ['Marto', 'Njugush', 'Kimani', 'Chege']
+
+const returnFirstTwoDrivers = () => drivers.slice(0,2)
+
+const returnLastTwoDrivers = () => drivers.slice(-2)
+
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+let createFareMultiplier = (x) => (fare) => x * fare;
+
+console.log(createFareMultiplier(4))    
+
+const fareDoubler = (createFareMultiplier) => createFareMultiplier*2;
+fareDoubler(35);
+
+const fareTripler = (createFareMultiplier) => createFareMultiplier*3;
+fareTripler(35);
+
+const selectDifferentDrivers = (drivers,returnFirstTwoDrivers) => returnFirstTwoDrivers(drivers);
